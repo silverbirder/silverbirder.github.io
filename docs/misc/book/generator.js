fe40accab49208e181d66f4db5e4a253f7578d18
shibamoto <silverbirder@gmail.com>
2021-12-21 15:25:59 +0900

    🙂 Delete unneed file

--- docs/misc/book/generator.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const json = JSON.parse(fs.readFileSync('./index.json', 'utf-8'));
-
-json.map((j) => {
-    const markdownFileName = `${j.title.replace(/\s/g, '_').replace(/\//g, '_').replace(/&/g, '_').replace(/'/g, '_')}.md`;
-    const markdownText = `# Book >> ${j.title}\n\n<img src="${j.cover}" style="width: 200px"/>`;
-    fs.writeFileSync(`./tmp/${markdownFileName}`, markdownText);
-});
--- docs/misc/goods/generator.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const json = JSON.parse(fs.readFileSync('./index.json', 'utf-8'));
-
-json.items.map((i) => {
-    const markdownFileName = `${i.name.replace(/\s/g, '_').replace(/\//g, '_').replace(/&/g, '_').replace(/'/g, '_')}.md`;
-    const markdownText = `# Goods >> ${i.name}\n\n<img src="${i.image}" style="width: 200px"/>`;
-    fs.writeFileSync(`./tmp/${markdownFileName}`, markdownText);
-});

8fefeb3835e098a04d7494c95ab95cdf0fc1fe41
shibamoto <silverbirder@gmail.com>
2023-09-09 23:09:26 +0900

    fix: 🐛 add blog title

--- apps/docs/src/routes/blog/index.tsx
+++
@@ -1,7 +0,0 @@
-import { component$ } from "@builder.io/qwik";
-import data from "./index.json";
-import { PostSummaryList } from "~/components/post-summary-list/post-summary-list";
-
-export default component$(() => {
-  return <PostSummaryList data={data}></PostSummaryList>;
-});

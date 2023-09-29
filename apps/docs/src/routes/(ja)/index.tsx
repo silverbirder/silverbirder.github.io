74e2d315a41d232f074e8ec26fac22c6e1feeb72
shibamoto <silverbirder@gmail.com>
2023-09-29 23:38:43 +0900

    feat: 🎸 update top page

--- apps/docs/src/routes/(ja)/index.tsx
+++ apps/docs/src/routes/(ja)/index.tsx
@@ -1,6 +1,13 @@
 import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
-import { MdiArrowDown } from "~/components/icon/icon";
+import { LogosGoogleCloudPlatform } from "~/components/icon/icon";
+import {
+  LogosJavascript,
+  LogosJest,
+  LogosPlaywright,
+  LogosStorybookIcon,
+  LogosWebcomponents,
+} from "~/components/icon/logos";
 import ImgFavicon from "~/media/favicon.svg?jsx";
 import { css } from "~/styled-system/css";
 
@@ -18,7 +25,7 @@ export default component$(() => {
           `,
         },
         gridTemplateColumns: {
-          base: "1fr 2fr",
+          base: "1fr 3fr",
           smDown: "auto",
         },
         gridTemplateRows: "auto",
@@ -36,55 +43,9 @@ export default component$(() => {
             height: "avatar.main",
           })}
         />
-        <h3>アイデアを形に</h3>
-        <section>
-          <h2>Biography</h2>
-          <p>
-            大学で初めてWebアプリケーション開発に挑戦し、アウトプットの喜びに触れ、これが今のキャリアのスタートとなりました。
-            アイディアが浮かべば、効率的かつコストパフォーマンスを考慮し、個人開発を進めています。
-            好きな食べ物は、ドーナツとチョコレートです。
-          </p>
-        </section>
-        <section
-          class={css({
-            display: "flex",
-            flexDirection: {
-              base: "column",
-              smDown: "column",
-            },
-            gap: 4,
-          })}
-        >
-          <div>
-            <h3>Mission</h3>
-            <p>お客様を日々の煩わしさから解放します。</p>
-          </div>
-          <div>
-            <h3>Vision</h3>
-            <p>
-              Web技術を活用し、皆様の生活の質を高め、より豊かなものとします。
-            </p>
-          </div>
-          <div>
-            <h3>Value</h3>
-            <p>
-              1. <strong>ユーザーファースト:</strong>
-              ユーザーのニーズと期待を最優先に考え、価値を提供します。
-            </p>
-            <p>
-              2. <strong>言語化:</strong>
-              複雑な問題も明瞭かつ正確に表現し、理解を深め、共有します。
-            </p>
-            <p>
-              3. <strong>シンプルイズベスト:</strong>
-              シンプルで直感的なデザインと機能で、使いやすさを追求します。
-            </p>
-            <p>
-              4. <strong>デリバリー後も品質保持:</strong>
-              プロダクトをリリースした後も、品質とサービスの向上を心掛けます。
-            </p>
-          </div>
-        </section>
+        <MessageSection />
+        <BiographySection />
+        <PreferencesSection />
       </div>
       <div
         class={css({
@@ -92,172 +53,189 @@ export default component$(() => {
           padding: "0 2rem",
         })}
       >
-        <section>
-          <h2>Work experience</h2>
-          <div
-            class={css({
-              display: "flex",
-              flexDirection: "column",
-            })}
-          >
-            <div
-              class={css({
-                display: "flex",
-                flexDirection: "row",
-                gap: 4,
-              })}
-            >
-              <p
-                class={css({
-                  backgroundColor: "bg.quote",
-                  borderRadius: "64px",
-                  height: "64px",
-                  width: "64px",
-                  padding: "10px",
-                })}
-              >
-                2016/04
-              </p>
-              <p>SI</p>
-            </div>
-            <MdiArrowDown
-              class={css({
-                borderRadius: "32px",
-                height: "32px",
-                width: "32px",
-              })}
-            />
-            <div
-              class={css({
-                display: "flex",
-                flexDirection: "row",
-                gap: 4,
-              })}
-            >
-              <p
-                class={css({
-                  backgroundColor: "bg.quote",
-                  borderRadius: "64px",
-                  height: "64px",
-                  width: "64px",
-                  padding: "10px",
-                })}
-              >
-                2017/08
-              </p>
-              <p>EC</p>
-            </div>
-            <MdiArrowDown
-              class={css({
-                borderRadius: "32px",
-                height: "32px",
-                width: "32px",
-              })}
-            />
-            <div
-              class={css({
-                display: "flex",
-                flexDirection: "row",
-                gap: 4,
-              })}
-            >
-              <p
-                class={css({
-                  backgroundColor: "bg.quote",
-                  borderRadius: "64px",
-                  height: "64px",
-                  width: "64px",
-                  padding: "10px",
-                })}
-              >
-                2022/01
-              </p>
-              <p>Fintech</p>
-            </div>
-            <MdiArrowDown
-              class={css({
-                borderRadius: "32px",
-                height: "32px",
-                width: "32px",
-              })}
-            />
-            <div
-              class={css({
-                display: "flex",
-                flexDirection: "row",
-                gap: 4,
-              })}
-            >
-              <p
-                class={css({
-                  backgroundColor: "bg.quote",
-                  borderRadius: "64px",
-                  height: "64px",
-                  width: "64px",
-                  padding: "10px",
-                })}
-              >
-                2022/07
-              </p>
-              <p>F&B</p>
-            </div>
-          </div>
-        </section>
-        <section>
-          <h2>Knowledges</h2>
-        </section>
-        <section>
-          <h2>Artifacts</h2>
-        </section>
+        <WorkExperiencesSection />
+        <KnowledgesSection />
+        <ArtifactsSection />
+        <MVCSection />
       </div>
     </div>
   );
 });
 
-/**
+const MessageSection = component$(() => {
+  return (
+    <section>
+      <h2>
+        アイデアを
+        <br />
+        形に
+      </h2>
+    </section>
+  );
+});
 
-<section>
-        <ImgFavicon />
-      </section>
-      <section>
-        <h2>Biography</h2>
-        <p>
-          2016年、Sler企業へ新卒入社し、Webアプリケーションの開発・保守・運用を経験。上流工程から下流工程まで幅広く業務を担当。
-          2018年、大手EC企業へ転職し、フルスタックエンジニア(データ,インフラ,アプリ)として、大規模アプリケーションのリアーキテクチャやモダナイゼーションに貢献。
-          2022年、FintechのSaaS企業へ転職し、フロントエンドエンジニアとして、既存プロダクト改修や新規プロダクト開発に携わり、今に至る。
-        </p>
-      </section>
+const BiographySection = component$(() => {
+  return (
+    <section>
+      <h3>Biography</h3>
+      <div>
+        大学で初めてWebアプリケーション開発に挑戦し、アウトプットの喜びに触れ、これが今のキャリアのスタートとなりました。
+        アイディアが浮かべば、効率的かつコストパフォーマンスを考慮し、個人開発を進めています。
+        好きな食べ物は、ドーナツとチョコレートです。
+      </div>
+    </section>
+  );
+});
 
-      <section>
-        <h2>Preferences</h2>
-        <p>これが好きだ！</p>
-      </section>
+const PreferencesSection = component$(() => {
+  return (
+    <section>
+      <h3>Preferences</h3>
+      <div
+        class={css({
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        })}
+      >
+        <LogosJavascript
+          class={css({ width: "icon.main", height: "icon.main" })}
+        />
+        <LogosJest class={css({ width: "icon.main", height: "icon.main" })} />
+        <LogosPlaywright
+          class={css({ width: "icon.main", height: "icon.main" })}
+        />
+        <LogosStorybookIcon
+          class={css({ width: "icon.main", height: "icon.main" })}
+        />
+        <LogosWebcomponents
+          class={css({ width: "icon.main", height: "icon.main" })}
+        />
+        <LogosGoogleCloudPlatform
+          class={css({ width: "icon.main", height: "icon.main" })}
+        />
+      </div>
+    </section>
+  );
+});
 
-      {/* <section>
-        <h2>MVC</h2>
+const WorkExperiencesSection = component$(() => {
+  return (
+    <section>
+      <h3>Work Experiences</h3>
+      <div
+        class={css({
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: {
+            base: "row",
+            smDown: "column",
+          },
+        })}
+      >
+        <WorkExperience
+          between="2016/04 ~ 2018/07"
+          industry="System Integration"
+        />
+        <WorkExperience between="2018/08 ~ 2021/12" industry="E-Commerce" />
+        <WorkExperience between="2022/01 ~ 2022/06" industry="Fintech" />
+        <WorkExperience between="2022/07 ~ now" industry="Restaurant" />
+      </div>
+    </section>
+  );
+});
+
+const WorkExperience = component$(
+  (props: { between: string; industry: string }) => {
+    return (
+      <div
+        class={css({
+          backgroundColor: "bg.quote",
+          margin: "8px",
+          padding: "8px",
+          width: {
+            base: "40%",
+            smDown: "100%",
+          },
+          borderRadius: 8,
+        })}
+      >
+        <h4>{props.between}</h4>
+        <div>{props.industry}</div>
+      </div>
+    );
+  }
+);
+
+const KnowledgesSection = component$(() => {
+  return (
+    <section>
+      <h3>Knowledges</h3>
+      <h4>Frontend</h4>
+      <h4>Backend</h4>
+      <h4>Infra</h4>
+      <h4>DevOps</h4>
+      <h4>Business</h4>
+    </section>
+  );
+});
+
+const ArtifactsSection = component$(() => {
+  return (
+    <section>
+      <h3>Artifacts</h3>
+    </section>
+  );
+});
+
+const MVCSection = component$(() => {
+  return (
+    <section
+      class={css({
+        display: "flex",
+        flexDirection: {
+          base: "column",
+          smDown: "column",
+        },
+        gap: 4,
+      })}
+    >
+      <div>
         <h3>Mission</h3>
-        <p>自分の使命</p>
+        <div>日々の煩わしさから解放します。</div>
+      </div>
+      <div>
         <h3>Vision</h3>
-        <p>
-          Web技術を通して、ユーザーの生活をより豊かにすることができる、そんなWebエンジニアを目指しています。
-        </p>
-        <h3>Values</h3>
-        <p>手段</p>
-      </section> 
-
-      <section>
-        <h2>CV</h2>
-        <h3>Work experience</h3>
-        <p>2016 ~ 何してた？</p>
-        <h3>Knowledges</h3>
-        <p>Javascriptとかね!</p>
-      </section>
-
-      <section>
-        <h2>Artifact</h2>
-      </section>
-
- */
+        <div>
+          Web技術を活用し、人々の生活の質を高め、より豊かなものとします。
+        </div>
+      </div>
+      <div>
+        <h3>Value</h3>
+        <ul>
+          <li>ユーザーファースト</li>
+          <ul>
+            <li>ユーザーのニーズと期待を最優先に考え、価値を提供します。</li>
+          </ul>
+          <li>言語化</li>
+          <ul>
+            <li>複雑な問題も明瞭かつ正確に表現し、理解を深め、共有します。</li>
+          </ul>
+          <li>シンプルイズベスト</li>
+          <ul>
+            <li>
+              シンプルで直感的なデザインと機能で、使いやすさを追求します。
+            </li>
+          </ul>
+          <li>QCD</li>
+          <ul>
+            <li>状況に応じて、QCDのバランスを考慮し、最適な判断をします。</li>
+          </ul>
+        </ul>
+      </div>
+    </section>
+  );
+});
 
 export const head: DocumentHead = ({ head }) => {
   return {

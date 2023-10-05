9a53a73a3129444494bcd2e9eeb13f67a614cc23
shibamoto <silverbirder@gmail.com>
2023-10-05 10:54:24 +0900

    refactor: 💡 refactor

---
+++ apps/docs/src/routes/(ja)/BiographySection.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+export const BiographySection = component$(() => {
+  return (
+    <section>
+      <h3>Biography</h3>
+      <div>
+        大学で初めてWebアプリケーション開発に挑戦し、アウトプットの喜びに触れ、これが今のキャリアのスタートとなりました。
+        アイディアが浮かべば、効率的かつコストパフォーマンスを考慮し、個人開発を進めています。
+        好きな食べ物は、ドーナツとチョコレートです。
+      </div>
+    </section>
+  );
+});
---
+++ apps/docs/src/routes/(ja)/MessageSection.tsx
@@ -0,0 +1,13 @@
+import { component$ } from "@builder.io/qwik";
+
+export const MessageSection = component$(() => {
+  return (
+    <section>
+      <h2>
+        アイデアを
+        <br />
+        形に
+      </h2>
+    </section>
+  );
+});

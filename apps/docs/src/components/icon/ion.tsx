424bea84621a0a58e7fdbb1936eda9398e5bba48
shibamoto <silverbirder@gmail.com>
2023-10-05 16:21:24 +0900

    feat: 🎸 Update blog layout

---
+++ apps/docs/src/components/icon/ion.tsx
@@ -0,0 +1,22 @@
+import { type QwikIntrinsicElements } from "@builder.io/qwik";
+
+export function IonLogoHackernews(
+  props: QwikIntrinsicElements["svg"],
+  key: string
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 512 512"
+      {...props}
+      key={key}
+    >
+      <path
+        fill="currentColor"
+        d="M32 32v448h448V32Zm249.67 250.83v84H235v-84l-77-140h55l46.32 97.54l44.33-97.54h52.73Z"
+      ></path>
+    </svg>
+  );
+}
--- apps/docs/src/routes/(ja)/blog/contents/layout.tsx
+++ apps/docs/src/routes/(ja)/blog/contents/layout.tsx
@@ -1,7 +1,18 @@
 import { component$, Slot } from "@builder.io/qwik";
 import { type DocumentHead, useDocumentHead } from "@builder.io/qwik-city";
+import { BuyMeACoffee } from "~/components/buy-me-a-coffee/buy-me-a-coffee";
+import { IonLogoHackernews } from "~/components/icon/ion";
+import {
+  SimpleIconsFacebook,
+  SimpleIconsHatenabookmark,
+  SimpleIconsLinkedin,
+  SimpleIconsMastodon,
+  SimpleIconsReddit,
+  SimpleIconsTwitter,
+} from "~/components/icon/simple";
 import { Tag } from "~/components/tag/tag";
 import data from "~/routes/(ja)/blog/index.json";
+import { css } from "~/styled-system/css";
 import { HStack } from "~/styled-system/jsx";
 
 export default component$(() => {
@@ -19,7 +30,125 @@ export default component$(() => {
         <Slot />
       </section>
       <section>
-        <h2>Tags</h2>
+        <div
+          class={css({
+            marginTop: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          })}
+        >
+          <div
+            class={css({
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            })}
+          >
+            <BuyMeACoffee />
+            <p
+              class={css({
+                fontSize: "0.8rem",
+                color: "gray.500",
+              })}
+            >
+              役立ったら、☕でサポートしてね！
+            </p>
+          </div>
+        </div>
+        <h2>Share</h2>
+        <div
+          class={css({
+            display: "flex",
+            flexDirection: "row",
+            gap: 1,
+          })}
+        >
+          <a
+            href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+              `${head.title} by @silverbirder | silverbirder.github.io ${head.frontmatter.permalink}`
+            )}`}
+            target="_blank"
+            class={css({ color: "blue.500", _hover: { color: "blue.900" } })}
+          >
+            <SimpleIconsTwitter
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+          <a
+            href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
+              head.frontmatter.permalink
+            )}&title=${encodeURIComponent(head.title)}`}
+            target="_blank"
+            class={css({ color: "blue.500", _hover: { color: "blue.900" } })}
+          >
+            <SimpleIconsLinkedin
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+          <a
+            href={`https://www.reddit.com/submit?url=${encodeURIComponent(
+              head.frontmatter.permalink
+            )}&title=${encodeURIComponent(head.title)}`}
+            target="_blank"
+            class={css({ color: "red.500", _hover: { color: "red.900" } })}
+          >
+            <SimpleIconsReddit
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+          <a
+            href={`https://news.ycombinator.com/submitlink?u=${encodeURIComponent(
+              head.frontmatter.permalink
+            )}&t=${encodeURIComponent(head.title)}`}
+            target="_blank"
+            class={css({
+              color: "orange.500",
+              _hover: { color: "orange.900" },
+            })}
+          >
+            <IonLogoHackernews
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+          <a
+            href={`https://www.facebook.com/sharer.php?u=${encodeURIComponent(
+              head.frontmatter.permalink
+            )}`}
+            target="_blank"
+            class={css({ color: "blue.500", _hover: { color: "blue.900" } })}
+          >
+            <SimpleIconsFacebook
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+          <a
+            href={`https://toot.kytta.dev/?text=${encodeURIComponent(
+              head.frontmatter.permalink
+            )}`}
+            target="_blank"
+            class={css({ color: "blue.500", _hover: { color: "blue.900" } })}
+          >
+            <SimpleIconsMastodon
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+          <a
+            href={`https://b.hatena.ne.jp/entry/panel/?url=${encodeURIComponent(
+              head.frontmatter.permalink
+            )}`}
+            target="_blank"
+            class={css({ color: "blue.500", _hover: { color: "blue.900" } })}
+          >
+            <SimpleIconsHatenabookmark
+              class={css({ width: "icon.main", height: "icon.main" })}
+            />
+          </a>
+        </div>
+      </section>
+      <section>
+        <h2>関連するタグ</h2>
         <RelatedTags currentTags={tags} currentTitle={head.title} />
       </section>
       <section>
@@ -84,6 +213,10 @@ export const head: DocumentHead = ({ head }) => {
   const permalink = currentPosts.length > 0 ? currentPosts[0].permalink : "";
   return {
     ...head,
+    frontmatter: {
+      ...head.frontmatter,
+      permalink: "https://silverbirder.github.io" + permalink,
+    },
     meta: [
       ...head.meta,
       { name: "twitter:card", content: "summary" },

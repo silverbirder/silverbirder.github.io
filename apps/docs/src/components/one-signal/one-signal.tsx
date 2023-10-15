508caefcb91f938508b199a8bc049886894998bf
shibamoto <silverbirder@gmail.com>
2023-10-15 22:35:57 +0900

    fix: 🐛 try one signal option

--- apps/docs/src/components/one-signal/one-signal.tsx
+++ apps/docs/src/components/one-signal/one-signal.tsx
@@ -21,64 +21,8 @@ export const OneSignal = component$<OneSignalProps>(() => {
           OneSignal.init({
             appId: "${appId}",
             safari_web_id: "${safariWebId}",
-            allowLocalhostAsSecureOrigin: true,
-            welcomeNotification: {
-              message: "${t("notification.welcome.message")}"
-            },
             notifyButton: {
-              enable: true,
-              size: 'small',
-              theme: 'default',
-              position: 'bottom-right',
-              showCredit: false,
-              text: {
-                  'tip.state.unsubscribed': "${t(
-                    "notification.tip.state.unsubscribed"
-                  )}",
-                  'tip.state.subscribed': "${t(
-                    "notification.tip.state.subscribed"
-                  )}",
-                  'tip.state.blocked': "${t("notification.tip.state.blocked")}",
-                  'message.prenotify': "${t("notification.message.prenotify")}",
-                  'message.action.subscribed': "${t(
-                    "notification.message.action.subscribe"
-                  )}",
-                  'message.action.resubscribed': "${t(
-                    "notification.message.action.resubscribed"
-                  )}",
-                  'message.action.unsubscribed': "${t(
-                    "notification.message.action.unsubscribed"
-                  )}",
-                  'dialog.main.title': "${t("notification.dialog.main.title")}",
-                  'dialog.main.button.subscribe': "${t(
-                    "notification.dialog.main.button.subscribe"
-                  )}",
-                  'dialog.main.button.unsubscribe': "${t(
-                    "notification.dialog.main.button.unsubscribe"
-                  )}",
-                  'dialog.blocked.title': "${t(
-                    "notification.dialog.blocked.title"
-                  )}",
-                  'dialog.blocked.message': "${t(
-                    "notification.dialog.blocked.message"
-                  )}"
-              },
-              colors: {
-                 'circle.background': '${token("colors.text.link")}',
-                 'circle.foreground': 'white',
-                 'badge.background': '${token("colors.text.link")}',
-                 'badge.foreground': 'white',
-                 'badge.bordercolor': 'white',
-                 'pulse.color': 'white',
-                 'dialog.button.background.hovering': '${token(
-                   "colors.text.linkActive"
-                 )}',
-                 'dialog.button.background.active': '${token(
-                   "colors.text.linkActive"
-                 )}',
-                 'dialog.button.background': '${token("colors.text.link")}',
-                 'dialog.button.foreground': 'white'
-              },
+              enable: true
             }
           })
         });

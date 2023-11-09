d7d367d8ac5c8431a99c9c18bed6cdcee5abe130
shibamoto <silverbirder@gmail.com>
2023-11-09 23:03:08 +0900

    refactor: 💡 refactor

--- apps/docs-e2e/features/internationalization/internationalization.feature
+++ apps/docs-e2e/features/internationalization/internationalization.feature
@@ -2,9 +2,9 @@ Feature: Internationalization
 
   Scenario Outline: Language Change
 
-    Given Alice navigates to the homepage in the <language> language
+    Given Alice is on the homepage in the <language> language
     When Alice selects <change_language> from the language menu
-    Then the page is displayed to Alice in the <change_language>
+    Then The page is displayed to her in the <change_language>
 
     Examples:
       | language | change_language |
--- apps/docs-e2e/features/screenplay/parameter.steps.ts
+++ apps/docs-e2e/features/screenplay/parameter.steps.ts
@@ -2,17 +2,18 @@ import { defineParameterType } from '@cucumber/cucumber';
 import { actorCalled, actorInTheSpotlight } from '@serenity-js/core';
 
 defineParameterType({
-    regexp: /[A-Z][a-z]+/,
+    regexp: /Alice|Bob/,
     transformer(name: string) {
         return actorCalled(name);
     },
     name: 'actor',
 });
 
+export type Language = 'Japanese' | 'English';
 defineParameterType({
     regexp: /Japanese|English/,
     transformer(name: string) {
-        return name;
+        return name as Language;
     },
     name: 'language',
 });

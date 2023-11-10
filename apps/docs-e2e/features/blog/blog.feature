03e2e1deda302a7f8770daceb585a4d702e8afdd
shibamoto <silverbirder@gmail.com>
2023-11-10 19:34:48 +0900

    style: 💄 fixed lint

--- apps/docs-e2e/features/blog/blog.feature
+++ apps/docs-e2e/features/blog/blog.feature
@@ -2,5 +2,5 @@ Feature: Blog
 
     Scenario Outline: Viewing a Blog Post
         Given Alice is on the blog list page
-        When she selects first blog post
+        When Alice selects first blog post
         Then Alice should see the selected blog post
--- apps/docs-e2e/features/blog/blog.steps.ts
+++ apps/docs-e2e/features/blog/blog.steps.ts
@@ -1,29 +1,29 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { Ensure, equals } from "@serenity-js/assertions";
-import { Actor, notes } from "@serenity-js/core";
-import { By, Navigate, PageElement, PageElements } from "@serenity-js/web";
+import { Given, Then, When } from '@cucumber/cucumber';
+import { Ensure, equals } from '@serenity-js/assertions';
+import { Actor, notes } from '@serenity-js/core';
+import { By, Navigate, PageElement, PageElements } from '@serenity-js/web';
 
-Given("{actor} is on the blog list page", async (actor: Actor) =>
-  actor.attemptsTo(Navigate.to("/blog"))
+Given('{actor} is on the blog list page', async (actor: Actor) =>
+    actor.attemptsTo(Navigate.to('/blog'))
 );
 
-When("{pronoun} selects first blog post", async (pronoun: Actor) =>
-  pronoun.attemptsTo(
-    notes().set(
-      "firstBlogPostTitle",
-      await PageElements.located(By.css(`[data-testid="blog-title"]`))
+When('{actor} selects first blog post', async (actor: Actor) =>
+    actor.attemptsTo(
+        notes().set(
+            'firstBlogPostTitle',
+            await PageElements.located(By.css(`[data-testid="blog-title"]`))
         .first()
         .text()
-    ),
-    PageElements.located(By.css(`[data-testid="blog-title"]`)).first().click()
-  )
+        ),
+        PageElements.located(By.css(`[data-testid="blog-title"]`)).first().click()
+    )
 );
 
-Then("{actor} should see the selected blog post", async (actor: Actor) =>
-  actor.attemptsTo(
-    Ensure.that(
-      await PageElement.located(By.tagName("h1")).text(),
-      equals(notes().get("firstBlogPostTitle"))
+Then('{actor} should see the selected blog post', async (actor: Actor) =>
+    actor.attemptsTo(
+        Ensure.that(
+            await PageElement.located(By.tagName('h1')).text(),
+            equals(notes().get('firstBlogPostTitle'))
+        )
     )
-  )
 );

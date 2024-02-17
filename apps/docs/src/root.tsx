8756daad3e435c07304816c1b1ada2b9bd715e1b
shibamoto <silverbirder@gmail.com>
2024-02-17 11:21:35 +0900

    feat: try GTM

--- apps/docs/src/root.tsx
+++ apps/docs/src/root.tsx
@@ -23,6 +23,15 @@ export default component$(() => {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <script
+            dangerouslySetInnerHTML="
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PDGFS772');
+            "
+          />
           <QwikPartytown forward={["gtag", "dataLayer.push"]} />
           <script
             async
@@ -75,6 +84,14 @@ export default component$(() => {
           <RouterHead />
         </head>
         <body lang="ja">
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PDGFS772"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <RouterOutlet />
           <ServiceWorkerRegister />
         </body>

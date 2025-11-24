6f5fb40e13a50c89777bfc60d86fb2f30051cd5d
shibamoto <silverbirder@gmail.com>
2025-11-24 14:55:07 +0900

    feat: add Bluesky logo component and SVG asset

---
+++ app/components/bluesky-logo.tsx
@@ -0,0 +1,9 @@
+export const BlueskyLogo = () => (
+  <a href="https://bsky.app/profile/silverbirder.bsky.social" target="_blank">
+    <img
+      src="/bluesky-brands-solid-full.svg"
+      alt="Bluesky"
+      className="h-5 w-5"
+    />
+  </a>
+);
